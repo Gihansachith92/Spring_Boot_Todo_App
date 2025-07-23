4c04503e603f4589f99db714b99d77d11c0e22df
Gihan_Sachith <gihansachith92@gmail.com>
2025-07-23 21:08:25 +0530

    creating logic of adding new task

--- src/main/java/com/app/todoapp/controller/TaskController.java
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -5,6 +5,8 @@ import com.app.todoapp.services.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -24,4 +26,11 @@ public class TaskController {
         model.addAttribute("tasks", tasks);
         return "tasks";
     }
+
+    @PostMapping
+    public String createTasks(@RequestParam String title){
+        taskService.createTask(title);
+        return "redirect:/";
+    }
+
 }
