811e45f2d4da932a412635231ed731fe4cada054
Gihan_Sachith <gihansachith92@gmail.com>
2025-07-27 15:43:09 +0530

    task delete functionality logic implementation

--- src/main/java/com/app/todoapp/controller/TaskController.java
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -5,6 +5,7 @@ import com.app.todoapp.services.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -30,6 +31,12 @@ public class TaskController {
     @PostMapping
     public String createTasks(@RequestParam String title){
         taskService.createTask(title);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteTasks(@PathVariable Long id){
+        taskService.deleteTask(id);
         return "redirect:/";
     }
 
